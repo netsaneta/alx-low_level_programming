691d42b996b157afdce79a4a0d13e45feb765ab3
Netsanet <free2.abayneh@gmail.com>
2022-09-08 16:35:59 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main function
+ */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle")
    return (0);
 }
