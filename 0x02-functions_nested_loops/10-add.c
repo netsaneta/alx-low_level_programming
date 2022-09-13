5d176765c5434a7d3a7a650eeac3d572d6d8b4b8
Netsanet <free2.abayneh@gmail.com>
2022-09-14 00:37:37 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check 
+ * @a: An integer x
+ * @b: An integer y
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
