90051f2ad6ae8900072ac2c4a52a814f3d19c34f
Netsanet <free2.abayneh@gmail.com>
2022-09-15 15:36:53 +0300

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9,
  * followed by a new line.
- * Return: Always 0 or nothing
+ * Return: nothing
  */
 void print_numbers(void)
 {
