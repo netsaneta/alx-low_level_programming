565a7da54aa4170d95882d0ba1713dcb94349087
Netsanet <free2.abayneh@gmail.com>
2022-10-04 23:40:28 +0300

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
